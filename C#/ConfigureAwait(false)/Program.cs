994ade3a0a56e6ef7f2cd59135c24329b68739ed
KhuongNC-Laptop <khuongNC9x@gmail.com>
2024-06-03 22:39:07 +0700

    Add ConfigureAwait(false)

---
+++ C#/ConfigureAwait(false)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigureAwait_false_
+{
+    internal class Program
+    {
+        public async static Task OldApproach()
+        {
+            await ReadDataAsync().ConfigureAwait(continueOnCapturedContext: true); // Potential deadlock if called from UI thread
+        }
+
+        public async static Task ImprovedApproach()
+        {
+            await ReadDataAsync().ConfigureAwait(false); // Avoids capturing the original context
+        }
+
+        private async static Task ReadDataAsync()
+        {
+            // Simulating an asynchronous I/O operation
+            await Task.Delay(1000); // Simulate a delay
+        }
+
+        static void Main(string[] args)
+        {
+            OldApproach();
+            ImprovedApproach();
+            Console.ReadLine();
+        }
+    }
+}
