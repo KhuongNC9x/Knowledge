50c9abe93a6beac4a867d16574b1cf3e43f33d1e
KhuongNC-Laptop <khuongNC9x@gmail.com>
2024-06-11 22:42:14 +0700

    Add Lazy Initialzation smaple

---
+++ C#/LazyInitialization/ExpensiveResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LazyInitialization
+{
+    public class ExpensiveResource
+    {
+        public ExpensiveResource()
+        {
+            Console.WriteLine("Initializing expensive resource... This might take a while.");
+            // Simulating expensive initialization with a delay
+            Thread.Sleep(2000); // Delay for 2 seconds
+            Console.WriteLine("Expensive resource initialized!");
+        }
+
+        public void UseResource()
+        {
+            Console.WriteLine("Using the expensive resource.");
+        }
+    }
+}
---
+++ C#/LazyInitialization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyInitialization
+{
+    internal class Program
+    {
+        ////Developers common mistake
+        //// Initializing expensive resources upfront
+        //private readonly static ExpensiveResource _expensiveObjectMistake = new ExpensiveResource();
+        //public ExpensiveResource ExpensiveObjectMistake => _expensiveObjectMistake;
+
+        // Efficient Method
+        // Using Lazy<T> to initialize resources only when needed
+        private readonly static Lazy<ExpensiveResource> _expensiveObject = new Lazy<ExpensiveResource>();
+        public ExpensiveResource ExpensiveObject => _expensiveObject.Value;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application started.");
+            Console.WriteLine("Press any key to use the expensive resource...");
+            Console.ReadKey();
+
+            // Accessing the Value property of _lazyExpensiveResource for the first time triggers the initialization.
+            _expensiveObject.Value.UseResource();
+
+            //_expensiveObjectMistake.UseResource();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
